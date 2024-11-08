65bb26a8d4fc41fb2862b7befd6d5e3cbffa84e4
Ethan Lowenthal <ethanmlowenthal@gmail.com>
2024-11-08 13:01:40 -0800

    second

---
+++ main.py
@@ -0,0 +1,28 @@
+from datetime import datetime, timedelta
+import subprocess
+
+def create_commit(date: datetime):
+    dateStr = date.strftime("%a %b %d %I:%M %Y +0700")
+    with open("out.txt", "a") as f:
+        f.write("a")
+    subprocess.run(["git", "add", "test.txt"])
+    subprocess.run(["git", "commit", "-m", "some stuff",])
+    subprocess.run(["git", "commit", "--amend", "-m", "some stuff", f'--date="{dateStr}"',])
+    
+def create_commits_for_date(numCommits: int, date: datetime):
+    dateStr = date.strftime("%a %b %d %I:%M %Y +0700")
+    print(f"Creating {numCommits} on {dateStr}")
+    for _ in range(numCommits):
+        create_commit(date)
+
+
+def get_commit_date(startDate, weeks, day):
+    return startDate + timedelta(weeks=weeks, days=day)
+
+if __name__ == "__main__":
+    commit_date = datetime.today() - timedelta(weeks=1, days=0)
+
+    num_commits = 1
+
+    print(f"Commit date: {commit_date.strftime('%m-%d-%Y')}, commits: {num_commits}")
+    create_commits_for_date(num_commits, commit_date)
